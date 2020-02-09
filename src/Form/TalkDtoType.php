0288685905a56d2e6cb0b8a105d12000881f1266
Dave Liddament <dave@lampbristol.com>
2020-02-09 22:39:35 +0000

    FIX few UI tweaks

--- src/Form/TalkDtoType.php
+++ src/Form/TalkDtoType.php
@@ -7,6 +7,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,7 +18,7 @@ class TalkDtoType extends AbstractType
     {
         $builder
             ->add('title', TextType::class)
-            ->add('abstract', TextType::class, [
+            ->add('abstract', TextareaType::class, [
                 'required' => false,
             ])
             ->add('videoUrl', TextType::class, [
