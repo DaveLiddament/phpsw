40034bf89320291176c982e5b6ff9497d48bba38
Dave Liddament <dave@lampbristol.com>
2020-02-09 19:05:39 +0000

    ADD unique constraints to entities

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -6,9 +6,11 @@ use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity()
+ * @UniqueEntity("slug")
  */
 class Event
 {
