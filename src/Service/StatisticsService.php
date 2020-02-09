abb6613b0b8562b58f84236dfcbdafeaec4eaec7
Dave Liddament <dave@lampbristol.com>
2020-02-09 18:46:29 +0000

    ADD service for grabbing current member count (this is just a dummy implementation at the moment)

---
+++ src/Service/StatisticsService.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+class StatisticsService
+{
+    public function getMembers(): int
+    {
+        // TODO do this properly
+        return 1012;
+    }
+}
